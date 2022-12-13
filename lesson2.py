cdb06c7e2b224ee9cbe7e50bddf77d527abdff5a
Mahdi <mahdirezaii490@gmail.com>
2022-12-13 09:42:13 +0000

    lesson2 added

---
+++ lesson2.py
@@ -0,0 +1,21 @@
+i = -4 # integer
+print(type(i))
+
+f = -7.5 # float
+print(type(f))
+
+s = 'hello' # string
+print(type(s))
+
+b = True
+b = False
+print(type(b))
+
+c = 12j
+print(type(c))
+
+i = 45.5
+print(type(i))
+
+age = input("please enter your age : ")
+print(type(age))
