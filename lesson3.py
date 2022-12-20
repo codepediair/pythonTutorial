1dc40ed19619f3b2584387f78fbd033dbb437044
Mahdi <mahdirezaii490@gmail.com>
2022-12-20 07:49:51 +0000

    lesson3 and lesson4 added

---
+++ lesson3.py
@@ -0,0 +1,25 @@
+# clean code
+myval = 15
+MyVal = 16
+my_val = 17
+myval2 = 18
+
+# my-val = 16
+# my val = 17
+# 2myval = 19
+
+i = 4
+print(float(i))
+print(str(i))
+
+z = 1
+# print(int(z))
+print(bool(z))
+
+# age = int(input("please enter your age: "))
+# age = int(age)
+# print(type(age))
+
+# CASTING
+
+print(bin(18465237))
---
+++ lesson4.py
@@ -0,0 +1,38 @@
+# operators
+# + * / -
+
+# x = int(input("x :"))
+# y = int(input("y :"))
+
+# print(x + y)
+# print(x - y)
+# print(x * y)
+# print(x / y)
+
+# ** % //
+# 
+# print(x ** y)
+# print(x % y)
+# print(x // y)
+# print(int(x / y))
+
+
+# print(x > y)
+# print(x < y)
+# print(x >= y)
+# print(x <= y)
+
+# print(x == y)
+# print(x != y)
+
+a = 3
+a += 3 # a = a + 3
+print(a)
+a -= 3
+a *= 3
+a /= 3
+
+a **= 3
+a %= 3
+a //= 3
+
