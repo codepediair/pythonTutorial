a43a17db13bc5cbd64296fa59e9efab8de72749e
mahdi <mahdirezaii490@gmail.com>
2022-12-31 18:56:44 +0330

    lesson 5 added

---
+++ basicConcepts/lesson5.py
@@ -0,0 +1,20 @@
+s = """hello world
+this is mahdi's book"""
+
+
+
+# print(s.upper())
+# print(s.lower())
+
+
+# print(s)
+# print(s[0:5:2])
+# print(s[::-1])
+
+# age = input("please enter your age: ")
+# print("hello" + " " + "world")
+# print("i am {} years old".format(age))
+# print(f"i am {age} years old")
+# print("i am %d years odl"% age)
+
+print(s.replace('h','z',2))
