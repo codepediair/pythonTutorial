513cb8c957de942135790cf320d8c0ae7b5f6aa2
mahdi <mahdirezaii490@gmail.com>
2023-01-08 20:28:34 +0330

    lesson 9 added

---
+++ basicConcepts/lesson6.py
@@ -0,0 +1,9 @@
+a = int(input("pleas enter a number : "))
+b = int(input("pleas enter a number again : "))
+
+if (a > b):
+    print(f"{a} > {b}")
+elif (a < b):
+    print(f"{b} > {a}")
+else:
+    print(f"{a} == {b}")
---
+++ basicConcepts/lesson7.py
@@ -0,0 +1,14 @@
+# i = 2 # start
+# while(i < 12): # condition
+#     # print("hello")
+#     print(i)
+#     i += 2 # step
+
+# for i in range(2,14,3):
+#     print(i)
+
+res = 0
+for i in range(1,1001):
+    res += i
+
+print(res)
