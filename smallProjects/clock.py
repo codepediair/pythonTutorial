78b2550aaeb4e7c34d45d12928ec94e11c03ff69
max <mahdirezaii490@gmail.com>
2023-08-22 14:06:38 +0330

    new projects added

---
+++ smallProjects/clock.py
@@ -0,0 +1,25 @@
+from tkinter import Label, Tk
+import time
+app_window = Tk()
+app_window.title("Digital Clock")
+app_window.geometry("490x150")
+app_window.resizable(1, 1)
+
+text_font = ("Boulder", 68, 'bold')
+background = "#ebab3f"
+foreground = "#1d1d2c"
+border_width = 25
+
+label = Label(app_window, font=text_font, bg=background,
+              fg=foreground, bd=border_width)
+label.grid(row=0, column=1)
+
+
+def digital_clock():
+    time_live = time.strftime("%H:%M:%S")
+    label.config(text=time_live)
+    label.after(200, digital_clock)
+
+
+digital_clock()
+app_window.mainloop()
