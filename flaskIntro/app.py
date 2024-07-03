3b107522bd09b6d35c6274e5a35ca4cacb4c5ad3
Mahdi <mahdirezaii490@gmail.com>
2024-07-03 11:31:21 +0330

    add html to flask

--- flaskIntro/app.py
+++ flaskIntro/app.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')#www.python.com
+@app.route('/index')
 def index():
-    return "Hello World"
+    user = {'username': 'codepedia'}
+    return render_template('index.html', user=user)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True) 
